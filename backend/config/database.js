2f1a30cfbad2b4b2bc50c3e5238f2a4e8d35734e
Sanjaya-Samudra <sanjayasamudra2003@gmail.com>
2025-03-09 18:47:21 +0530

    Full Project - 01

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'shav@3490',
+  password: '2003',
   database: 'secureconnect'
 });
 
